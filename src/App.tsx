457e1bf84133e198c26e74423e1e9856eb0d9694
Nicolas Justen <nicolas.justen@sivis.com>
2024-01-18 16:12:40 +0100

    create basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import './App.css';
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Body from "./components/body/Body";
+import {Box} from "@mui/material";
 
 function App() {
     return (
-        <div className="App">
+        <Box
+            minHeight={'100vh'}
+            display={'flex'}
+            flexDirection={'column'}>
             <Header/>
             <Body/>
             <Footer/>
-        </div>
+        </Box>
     );
 }
 
