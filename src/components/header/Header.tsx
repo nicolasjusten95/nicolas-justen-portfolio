aa52bbb5f9b01f1749e48035058344abaea87945
Nicolas Justen <nicolas.justen@sivis.com>
2024-01-18 15:10:29 +0100

    add basic website structure

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,114 @@
+import {
+    AppBar,
+    Box,
+    Button,
+    IconButton,
+    ListItemIcon,
+    ListItemText,
+    Menu,
+    MenuItem,
+    MenuList,
+    Stack,
+    Toolbar,
+    Typography
+} from "@mui/material";
+import BarChartIcon from '@mui/icons-material/BarChart';
+import MenuIcon from '@mui/icons-material/Menu';
+import React, {MouseEvent, useState} from "react";
+import SearchIcon from '@mui/icons-material/Search';
+import {Page} from "../../api/NavBarApi";
+
+
+interface HeaderProps {
+}
+
+const Header = (props: HeaderProps) => {
+
+    const [anchorNav, setAnchorNav] = useState<null | HTMLElement>(null);
+
+    const onOpenMenu = (event: MouseEvent<HTMLElement>): void => {
+        setAnchorNav(event.currentTarget);
+    };
+
+    const onCloseMenu = (): void => {
+        setAnchorNav(null);
+    };
+
+    const title: string = 'Nicolas Justen';
+    const icon: React.ReactElement = <BarChartIcon/>;
+    const pages: Page [] = [
+        {name: 'Test', icon: <SearchIcon/>, onClick: () => null, key: 1},
+    ];
+
+    const getButtonFromPage = (page: Page): React.ReactElement => {
+        return <Button
+            key={page.key}
+            startIcon={page.icon}
+            onClick={page.onClick}
+            color='inherit'>
+            <Typography>{page.name}</Typography>
+        </Button>;
+    };
+
+    const getMenuItemFromPage = (page: Page): React.ReactElement => {
+        return <MenuItem
+            key={page.key}
+            onClick={page.onClick}>
+            <ListItemIcon>{page.icon}</ListItemIcon>
+            <ListItemText>
+                <Typography>{page.name}</Typography>
+            </ListItemText>
+        </MenuItem>
+    };
+
+    return (
+        <AppBar
+            position='static'
+            sx={{mb: 2}}>
+            <Toolbar>
+                <IconButton
+                    size='large'
+                    edge='start'
+                    color='inherit'
+                    aria-label='logo'
+                    onClick={() => null}>
+                    {icon}
+                </IconButton>
+                <Typography
+                    variant='h6'
+                    component='div'
+                    sx={{flexGrow: 1}}>
+                    {title}
+                </Typography>
+                {/*Desktop*/}
+                <Stack
+                    sx={{display: {xs: 'none', md: 'flex'}}}
+                    direction='row'
+                    spacing={2}>
+                    {pages.map((page) => getButtonFromPage(page))}
+                </Stack>
+                {/*Mobile*/}
+                <Box sx={{display: {xs: 'flex', md: 'none'}}}>
+                    <IconButton
+                        size='large'
+                        edge='start'
+                        color='inherit'
+                        onClick={onOpenMenu}>
+                        <MenuIcon/>
+                    </IconButton>
+                    <Menu
+                        open={Boolean(anchorNav)}
+                        onClose={onCloseMenu}
+                        anchorEl={anchorNav}
+                        sx={{display: {xs: 'flex', md: 'none'}}}>
+                        <MenuList>
+                            {pages.map((page) => getMenuItemFromPage(page))}
+                        </MenuList>
+                    </Menu>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Header;
