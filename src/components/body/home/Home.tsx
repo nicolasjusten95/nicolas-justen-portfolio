57da96856a858f24e68d2677f09bd50214f9db39
Nicolas Justen <nicolas.justen@sivis.com>
2024-01-31 16:36:24 +0100

    add links to biography

--- src/components/body/home/Home.tsx
+++ src/components/body/home/Home.tsx
@@ -1,14 +1,5 @@
 import {Avatar, Box, Link, Stack, Typography} from "@mui/material";
 import profilePicture from '../../../images/profile_picture.jpg';
-import {
-    currentCompany,
-    currentCompanyLink,
-    heading,
-    jobTitle,
-    name,
-    paragraph1,
-    paragraph2
-} from "../../../data/HomeData";
 import {githubIcon, linkedInIcon, mailIcon} from "../../../data/SocialMediaIcons";
 
 
@@ -25,19 +16,18 @@ const Home = () => {
                 alignItems='center'
                 width='20%'>
                 <Avatar
-                    alt={name}
+                    alt={"Nicolas Justen"}
                     src={profilePicture}
                     sx={{width: 'auto', height: 'auto'}}/>
-                <Typography color='text.primary' variant='h4' p='8px'>{name}</Typography>
-                <Typography color='text.primary' variant='h6'>{jobTitle}</Typography>
+                <Typography color='text.primary' variant='h4' p='8px'>Nicolas Justen</Typography>
+                <Typography color='text.primary' variant='h6'>Software Developer</Typography>
                 <Typography color='text.primary' variant='h6'>
                     <Link
-                        href={currentCompanyLink}
+                        href='https://www.sivis.com/'
                         color='text.primary'
-                        underline='hover'
                         target="_blank"
                         rel="noreferrer">
-                        {currentCompany}
+                        SIVIS GmbH
                     </Link>
                 </Typography>
                 <Stack direction='row' p='8px'>
@@ -47,9 +37,54 @@ const Home = () => {
                 </Stack>
             </Box>
             <Box width='70%'>
-                <Typography color='text.primary' variant='h4' pb='15px'>{heading}</Typography>
-                <Typography color='text.primary' variant='h6' pb='15px'>{paragraph1}</Typography>
-                <Typography color='text.primary' variant='h6'>{paragraph2}</Typography>
+                <Typography color='text.primary' variant='h4' pb='15px'>Biography</Typography>
+                <Typography color='text.primary' variant='h6' pb='15px'>
+                    I am a Software Developer with over two years of experience in shipping core features and
+                    algorithms. My current employer
+                    {<Link href='https://www.sivis.com/'
+                           color='text.primary'
+                           target="_blank"
+                           rel="noreferrer">
+                        &nbsp;SIVIS GmbH&nbsp;
+                    </Link>}
+                    {<Link href='https://www.pointsharp.com/'
+                           color='text.primary'
+                           target="_blank"
+                           rel="noreferrer">
+                        &nbsp;(part of Pointsharp)&nbsp;
+                    </Link>}
+                    is focusing on enterprise-level identity & access management software in cyber security solutions
+                    that is being deployed by over 3,000 corporations across Europe. In my role I am developing full
+                    stack web applications, mostly using Spring Boot and React. My main work is on the
+                    {<Link href='https://www.sivis.com/en/product/sivis-authorization-robot'
+                           color='text.primary'
+                           target="_blank"
+                           rel="noreferrer">
+                        &nbsp;Authorization Robot
+                    </Link>}
+                    , a product that automizes the creation of ERP authorization concepts.
+                </Typography>
+                <Typography color='text.primary' variant='h6'>
+                    Prior to joining SIVIS, I got my Master of Science as well as my Bachelor of Science in Business
+                    Administration and Engineering from the
+                    {<Link href='https://www.h-ka.de/en/'
+                           color='text.primary'
+                           target="_blank"
+                           rel="noreferrer">
+                        &nbsp;University of Applied Sciences Karlsruhe
+                    </Link>}
+                    , where I focused mainly on digitalisation. During my Master's thesis I focused on the integration
+                    of user interactions for the Role Mining Problem into Evolutionary Algorithms and published the
+                    results in the
+                    {<Link href='https://www.scitepress.org/PublicationsDetail.aspx?ID=VO0asNCzymE=&t=1'
+                           color='text.primary'
+                           target="_blank"
+                           rel="noreferrer">
+                        &nbsp;Proceedings of the 15th International Joint Conference on Computational Intelligence -
+                        ECTA
+                    </Link>}
+                    .
+                </Typography>
             </Box>
         </Box>
     );
