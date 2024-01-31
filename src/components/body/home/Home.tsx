83ef50e70d5dbfb8673d3893ab2c5b18c6a6f683
Nicolas Justen <nicolas.justen@sivis.com>
2024-01-31 16:06:37 +0100

    add biography section

--- src/components/body/home/Home.tsx
+++ src/components/body/home/Home.tsx
@@ -1,6 +1,14 @@
 import {Avatar, Box, Link, Stack, Typography} from "@mui/material";
 import profilePicture from '../../../images/profile_picture.jpg';
-import {currentCompany, currentCompanyLink, jobTitle, name} from "../../../data/HomeData";
+import {
+    currentCompany,
+    currentCompanyLink,
+    heading,
+    jobTitle,
+    name,
+    paragraph1,
+    paragraph2
+} from "../../../data/HomeData";
 import {githubIcon, linkedInIcon, mailIcon} from "../../../data/SocialMediaIcons";
 
 
@@ -8,29 +16,41 @@ const Home = () => {
     return (
         <Box
             display='flex'
-            flexDirection='column'
-        alignItems='center'>
-            <Avatar
-                alt={name}
-                src={profilePicture}
-                sx={{ width: 256, height: 256 }}/>
-            <Typography color='text.primary' variant='h4' p='8px'>{name}</Typography>
-            <Typography color='text.primary' variant='h6'>{jobTitle}</Typography>
-            <Typography color='text.primary' variant='h6'>
-                <Link
-                    href={currentCompanyLink}
-                    color='text.primary'
-                    underline='hover'
-                    target="_blank"
-                    rel="noreferrer">
-                    {currentCompany}
-                </Link>
-            </Typography>
-            <Stack direction='row'>
-                {mailIcon}
-                {linkedInIcon}
-                {githubIcon}
-            </Stack>
+            flexDirection='row'
+            justifyContent='space-between'
+            width='80%'>
+            <Box
+                display='flex'
+                flexDirection='column'
+                alignItems='center'
+                width='20%'>
+                <Avatar
+                    alt={name}
+                    src={profilePicture}
+                    sx={{width: 'auto', height: 'auto'}}/>
+                <Typography color='text.primary' variant='h4' p='8px'>{name}</Typography>
+                <Typography color='text.primary' variant='h6'>{jobTitle}</Typography>
+                <Typography color='text.primary' variant='h6'>
+                    <Link
+                        href={currentCompanyLink}
+                        color='text.primary'
+                        underline='hover'
+                        target="_blank"
+                        rel="noreferrer">
+                        {currentCompany}
+                    </Link>
+                </Typography>
+                <Stack direction='row' p='8px'>
+                    {mailIcon}
+                    {linkedInIcon}
+                    {githubIcon}
+                </Stack>
+            </Box>
+            <Box width='70%'>
+                <Typography color='text.primary' variant='h4' pb='15px'>{heading}</Typography>
+                <Typography color='text.primary' variant='h6' pb='15px'>{paragraph1}</Typography>
+                <Typography color='text.primary' variant='h6'>{paragraph2}</Typography>
+            </Box>
         </Box>
     );
 };
--- src/data/HomeData.ts
+++ src/data/HomeData.ts
@@ -1,4 +1,15 @@
 export const name = "Nicolas Justen";
 export const jobTitle = "Software Developer";
-export const currentCompany = "Sivis GmbH";
+export const currentCompany = "SIVIS GmbH";
 export const currentCompanyLink = "https://www.sivis.com/";
+export const heading = "Biography";
+export const paragraph1 = `I am a Software Developer with over two years of experience in 
+    shipping core features and algorithms. My current employer SIVIS GmbH (part of Pointsharp) is focusing on 
+    enterprise-level identity & access management software in cyber security solutions that is being deployed by over 
+    3,000 corporations across Europe. In my role I am developing full stack web applications, mostly using Spring Boot 
+    and React.`;
+export const paragraph2 = `Prior to joining SIVIS, I got my Master of Science as well as my 
+    Bachelor of Science in Business Administration and Engineering from the University of Applied Sciences Karlsruhe, 
+    where I focused mainly on digitalisation. During my Master's thesis I focused on the integration of user 
+    interactions for the Role Mining Problem into Evolutionary Algorithms and published the results in a research paper.
+    `;
