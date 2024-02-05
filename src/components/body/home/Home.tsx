6a5e4f2ef83f295242d12d9b2cb1e38475bc5d68
Nicolas Justen <nicolas.justen@sivis.com>
2024-02-05 15:41:54 +0100

    adapt home to mobile

--- src/components/body/home/Home.tsx
+++ src/components/body/home/Home.tsx
@@ -25,9 +25,18 @@ const Home = () => {
                 <Avatar
                     alt={"Nicolas Justen"}
                     src={profilePicture}
-                    sx={{width: 'auto', height: 'auto'}}/>
-                <Typography color='text.primary' variant='h4' m='8px'>Nicolas Justen</Typography>
-                <Typography color='text.primary' variant='h6'>Software Developer</Typography>
+                    sx={{width: 'auto', height: 'auto', maxWidth: '300px'}}/>
+                <Typography
+                    color='text.primary'
+                    m='8px'
+                    sx={{typography: {xs: 'h5', lg: 'h4'}}}>
+                    Nicolas Justen
+                </Typography>
+                <Typography
+                    color='text.primary'
+                    variant='h6'>
+                    Software Developer
+                </Typography>
                 <Typography color='text.primary' variant='h6'>
                     <MyLink href='https://www.sivis.com/' text='SIVIS GmbH'/>
                 </Typography>
@@ -35,27 +44,38 @@ const Home = () => {
             </Box>
             <Box
                 sx={{
-                width: {xs: '100%', md: '65%'},
-                mt: {xs: '40px', md: '0px'}
-            }}>
-                <Typography color='text.primary' variant='h4' mb='20px'>Hello! 👋</Typography>
-                <Typography color='text.primary' variant='h6' mb='20px'>
+                    width: {xs: '100%', md: '65%'},
+                    mt: {xs: '40px', md: '0px'}
+                }}>
+                <Typography
+                    color='text.primary'
+                    mb='20px'
+                    sx={{typography: {xs: 'h5', md: 'h4'}}}>
+                    Hello! 👋
+                </Typography>
+                <Typography
+                    color='text.primary'
+                    mb='20px'
+                    sx={{typography: {xs: 'body1', md: 'h6'}}}>
                     I'm Nicolas, a passionate Software Developer with over two years of experience in shipping
                     core features and algorithms. At&nbsp;
                     <MyLink href='https://www.sivis.com/' text='SIVIS GmbH'/>
-                    &nbsp;
-                    <MyLink href='https://www.pointsharp.com/' text='(part of Pointsharp)'/>
-                    &nbsp;I'm focusing on enterprise-level identity & access management software in cyber security
-                    solutions that is being deployed by over 3,000 corporations across Europe. My journey with
-                    developing full-stack web applications has me weaving through the intricacies of Spring Boot and
-                    React to contribute to our flagship product: the&nbsp;
+                    &nbsp;(part of&nbsp;
+                    <MyLink href='https://www.pointsharp.com/' text='Pointsharp'/>
+                    ) I'm focusing on enterprise-level identity & access management software within cyber security
+                    solutions that is being deployed by over 3,000 corporations across Europe. My technical expertise
+                    centers around developing full-stack web applications using mostly Java and Spring Boot, playing a
+                    pivotal role in contributing to our flagship product: the&nbsp;
                     <MyLink
                         href='https://www.sivis.com/en/product/sivis-authorization-robot'
                         text='Authorization Robot'/>
-                    . This tool is revolutionizing the way company's create and maintain ERP authorization concepts by
-                    fully automating this process.
+                    . This innovative tool is revolutionizing the way companies create and maintain ERP authorization
+                    concepts by fully automating this process.
                 </Typography>
-                <Typography color='text.primary' variant='h6' mb='20px'>
+                <Typography
+                    color='text.primary'
+                    mb='20px'
+                    sx={{typography: {xs: 'body1', md: 'h6'}}}>
                     Prior to joining SIVIS my academic path led me through the&nbsp;
                     <MyLink href={'https://www.h-ka.de/'} text={"University of Applied Sciences Karlsruhe"}/>
                     , where I earned both my Bachelor's and Master's degrees in Business Administration and Engineering
@@ -71,10 +91,12 @@ const Home = () => {
                         text='15th International Joint Conference on Computational Intelligence - ECTA'/>
                     .
                 </Typography>
-                <Typography color='text.primary' variant='h6'>
+                <Typography
+                    color='text.primary'
+                    sx={{typography: {xs: 'body1', md: 'h6'}}}>
                     When I'm not immersed in code, I seek refuge in the rhythm of music and the vastness of nature.
                     Whether I'm strumming to the classics on my electric guitar, chasing the horizon running on forest
-                    trails or carving through snowy mountains on my skis, life is all about having a good time for
+                    trails, or carving through snowy mountains on my skis, life is all about having a great time for
                     me. Traveling is another avenue where I satisfy my curiosity, immersing myself in diverse cultures
                     and collecting stories along the way.
                 </Typography>
